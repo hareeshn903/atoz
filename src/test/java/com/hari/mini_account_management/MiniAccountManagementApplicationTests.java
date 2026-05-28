249a21acae010b0386f88e0c04e5c6a26da180da
hareeshn903 <hareeshn903@gmail.com>
2026-05-28 22:26:51 +0530

    updated maven3

--- src/test/java/com/hari/mini_account_management/MiniAccountManagementApplicationTests.java
+++ src/test/java/com/hari/mini_account_management/MiniAccountManagementApplicationTests.java
@@ -2,7 +2,9 @@ package com.hari.mini_account_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class MiniAccountManagementApplicationTests {
 
